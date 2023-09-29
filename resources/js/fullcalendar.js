32ddf8a56ef0ad7954e57a20cd83bc49cf4693ab
Not-Applicable-NA <shomcdremi.s@gmail.com>
2023-09-30 02:15:56 +0900

    fullcalendar導入

---
+++ resources/js/fullcalendar.js
@@ -0,0 +1,9 @@
+import { Calendar } from "fullcalendar";
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
