e55c72fab622a9b5913b04b5b5d92a3880a4eeb9
Not-Applicable-NA <shomcdremi.s@gmail.com>
2023-10-28 23:55:26 +0900

    ClassNumberモデル作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentSeeder::class,
             MajorSeeder::class,
-            ClassTypeSeeder::class
+            ClassTypeSeeder::class,
+            ClassNumberSeeder::class
         ]);
     }
 }
