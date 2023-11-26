4de1b12144be97f5e4ff67930b855bcb8a162458
Not-Applicable-NA <shomcdremi.s@gmail.com>
2023-11-26 17:46:10 +0900

    先端経営学科科目テーブルと教員テーブルの中間テーブルを作成

--- app/Models/BusinessSubject.php
+++ app/Models/BusinessSubject.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class BusinessSubject extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    public function teachers(): BelongsToMany
+    {
+        return $this->belongsToMany(Teacher::class);
+    }
 }
