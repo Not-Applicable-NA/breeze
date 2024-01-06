ac2daec63eee36d0c5dcd05ca0b1c9fab749fa35
Not-Applicable-NA <shomcdremi.s@gmail.com>
2024-01-06 18:00:32 +0900

    Googleカレンダーからイベントを取得するテスト

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Spatie\GoogleCalendar\Event;
 
 class DashboardController extends Controller
 {
     public function show(): View
     {
+        $events = Event::get();
+        foreach ($events as $event) {
+            dump($event);
+        }
+        dd();
         return view('dashboard');
     }
 }
