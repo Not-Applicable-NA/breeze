219abd627ac07f0bc404f27a46301fc83789ed56
Not-Applicable-NA <shomcdremi.s@gmail.com>
2024-02-07 17:07:52 +0900

    科目一覧をスクロール可能にする

--- resources/views/subjects/subjects.blade.php
+++ resources/views/subjects/subjects.blade.php
@@ -183,80 +183,82 @@
                                     登録
                                 </x-primary-button>
                             </div>
-                            <table class="mt-4 w-full border-collapse text-center">
-                                <thead>
-                                    <tr>
-                                        <th class="p-2">科目名</th>
-                                        <th class="p-2">単位数</th>
-                                        <th class="p-2">配当年次</th>
-                                        <th class="p-2">必修科目</th>
-                                        <th class="p-2">開講学期</th>
-                                        <th class="p-2">開講日時</th>
-                                        <th class="p-2">主教室</th>
-                                        <th class="p-2">担当教員</th>
-                                        <th class="p-2">対象クラス</th>
-                                        <th class="p-2">シラバス</th>
-                                        <th class="p-2"></th>
-                                        <th class="p-2"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ( $subjects as $subject )
+                            <div class="overflow-auto h-[720px]">
+                                <table class="mt-4 w-full border-collapse text-center whitespace-nowrap">
+                                    <thead>
                                         <tr>
-                                            <td class="border-t p-2">
-                                                <input id="subject_{{ $subject->id }}" type="checkbox" name="subject[]" value="{{ $subject->id }}" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500">
-                                                <label for="subject_{{ $subject->id }}" class="ml-1">{{ $subject->name }}</label>
-                                            </td>
-                                            <td class="border-t p-2">{{ $subject->credits }}単位</td>
-                                            <td class="border-t p-2">{{ $subject->dividend_grade }}年</td>
-                                            <td class="border-t p-2">
-                                                @if ($subject->is_obligatory)
-                                                    ○
-                                                @endif
-                                            </td>
-                                            <td class="border-t p-2">{{ $subject->semester }}</td>
-                                            <td class="border-t p-2">
-                                                {{ $dayOfWeeks[$subject->day_of_week_1] }}{{ $subject->period_1 }}@if ($subject->is_in_a_row_1), {{ $subject->period_1+1 }}@endif
-                                                @if ( $subject->day_of_week_2 )
-                                                    <br> {{ $dayOfWeeks[$subject->day_of_week_2] }}{{ $subject->period_2 }}@if ($subject->is_in_a_row_2), {{ $subject->period_2+1 }}@endif
-                                                @endif
-                                            </td>
-                                            <td class="border-t p-2">{{ $subject->main_lecture_room }}</td>
-                                            <td class="border-t p-2">
-                                                @foreach ( $subject->teachers as $teacherInCharge )
-                                                    {{ $teacherInCharge->name }}<br>
-                                                @endforeach
-                                            </td>
-                                            <td class="border-t p-2">
-                                                @if ($subject->classes->isNotEmpty())
-                                                    @foreach ( $subject->classes as $targetCasses )
-                                                        {{ $targetCasses->name }}<br>
-                                                    @endforeach
-                                                @else
-                                                    @foreach ( $classes as $class )
-                                                        {{ $class->name }}<br>
-                                                    @endforeach
-                                                @endif
-                                            </td>
-                                            <td class="border-t p-2">{{ $subject->syllabus }}</td>
-                                            <td class="border-t p-2">
-                                                <div class="flex justify-center items-center">
-                                                    <a href="{{ route('subjects.edit', ['id' => $subject->id]) }}" class="'inline-flex items-center px-4 py-2 bg-slate-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'">
-                                                        編集
-                                                    </a>
-                                                </div>
-                                            </td>
-                                            <td class="border-t p-2">
-                                                <div class="flex justify-center items-center">
-                                                    <a href="{{ route('subjects.delete', ['id' => $subject->id]) }}" class="'inline-flex items-center px-4 py-2 bg-slate-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'">
-                                                        削除
-                                                    </a>
-                                                </div>
-                                            </td>
+                                            <th class="p-2">科目名</th>
+                                            <th class="p-2">単位数</th>
+                                            <th class="p-2">配当年次</th>
+                                            <th class="p-2">必修科目</th>
+                                            <th class="p-2">開講学期</th>
+                                            <th class="p-2">開講日時</th>
+                                            <th class="p-2">主教室</th>
+                                            <th class="p-2">担当教員</th>
+                                            <th class="p-2">対象クラス</th>
+                                            <th class="p-2">シラバス</th>
+                                            <th class="p-2"></th>
+                                            <th class="p-2"></th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ( $subjects as $subject )
+                                            <tr>
+                                                <td class="border-t p-2">
+                                                    <input id="subject_{{ $subject->id }}" type="checkbox" name="subject[]" value="{{ $subject->id }}" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500">
+                                                    <label for="subject_{{ $subject->id }}" class="ml-1">{{ $subject->name }}</label>
+                                                </td>
+                                                <td class="border-t p-2">{{ $subject->credits }}単位</td>
+                                                <td class="border-t p-2">{{ $subject->dividend_grade }}年</td>
+                                                <td class="border-t p-2">
+                                                    @if ($subject->is_obligatory)
+                                                        ○
+                                                    @endif
+                                                </td>
+                                                <td class="border-t p-2">{{ $subject->semester }}</td>
+                                                <td class="border-t p-2">
+                                                    {{ $dayOfWeeks[$subject->day_of_week_1] }}{{ $subject->period_1 }}@if ($subject->is_in_a_row_1), {{ $subject->period_1+1 }}@endif
+                                                    @if ( $subject->day_of_week_2 )
+                                                        <br> {{ $dayOfWeeks[$subject->day_of_week_2] }}{{ $subject->period_2 }}@if ($subject->is_in_a_row_2), {{ $subject->period_2+1 }}@endif
+                                                    @endif
+                                                </td>
+                                                <td class="border-t p-2">{{ $subject->main_lecture_room }}</td>
+                                                <td class="border-t p-2">
+                                                    @foreach ( $subject->teachers as $teacherInCharge )
+                                                        {{ $teacherInCharge->name }}<br>
+                                                    @endforeach
+                                                </td>
+                                                <td class="border-t p-2">
+                                                    @if ($subject->classes->isNotEmpty())
+                                                        @foreach ( $subject->classes as $targetCasses )
+                                                            {{ $targetCasses->name }},
+                                                        @endforeach
+                                                    @else
+                                                        @foreach ( $classes as $class )
+                                                            {{ $class->name }}, 
+                                                        @endforeach
+                                                    @endif
+                                                </td>
+                                                <td class="border-t p-2 whitespace-normal">{{ $subject->syllabus }}</td>
+                                                <td class="border-t p-2">
+                                                    <div class="flex justify-center items-center">
+                                                        <a href="{{ route('subjects.edit', ['id' => $subject->id]) }}" class="'inline-flex items-center px-4 py-2 bg-slate-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'">
+                                                            編集
+                                                        </a>
+                                                    </div>
+                                                </td>
+                                                <td class="border-t p-2">
+                                                    <div class="flex justify-center items-center">
+                                                        <a href="{{ route('subjects.delete', ['id' => $subject->id]) }}" class="'inline-flex items-center px-4 py-2 bg-slate-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'">
+                                                            削除
+                                                        </a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                             <x-input-error :messages="$errors->get('subject')" class="mt-2" />
                         </form>
                     </div>
